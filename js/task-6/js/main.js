df5c085c5c49ff453b4dbca4286e8d70db39c3ca
marianasvirzhevska <mariana.svirzhevska@nordwhale.com>
2019-12-25 18:02:34 +0200

    Task 6: Posts app

--- js/task-6/js/main.js
+++ js/task-6/js/main.js
@@ -1,108 +1,146 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const body = document.querySelector('body');
+	const container = document.querySelector('.container');
 
-	let posts = JSON.parse(localStorage.getItem('posts') || '{}');
-	let users = JSON.parse(localStorage.getItem('users') || '{}');
+	let usersData;
+	let postsData;
 
-	getPosts();
-	getUsers();
+	Promise.all([getPosts(), getUsers()])
+		.then(([posts, users]) => {
+			renderPosts(posts, users)
+		});
 
-	renderPosts(posts);
+	function renderPosts(posts, users) {
+		clear();
+		const list = createPostsList(posts, users);
+		container.appendChild(list);
+	}
 
-	function renderPosts(posts){
-		let ul = document.createElement('ul');
-		posts.map(el => {
-			let li = document.createElement('li');
-			let a = document.createElement('a');
-			let user = document.createElement('span');
-			a.appendChild(document.createTextNode(el.title));
+	function renderPost(post, author, comments) {
+		clear();
+		goBack();
+		const el = createPostElement(post, author, comments);
+		container.appendChild(el);
+	}
+
+	function clear() {
+		container.innerHTML = '';
+	}
+
+
+	// DOM elements
+
+	function goBack(){
+		const button = document.createElement('button');
+		button.innerText = 'Back to posts';
+		button.addEventListener('click', () => {
+			clear();
+			renderPosts(postsData, usersData);
+		});
+		container.appendChild(button);
+	}
+
+	function createPostsList(posts, users) {
+		const ul = document.createElement('ul');
+
+		posts.map(post => {
+			const li = document.createElement('li');
+			const a = document.createElement('a');
+
+			const author = users[post.userId];
+
+			a.innerHTML = `<b>${post.title}</b> Author: <i>${author.name}</i>`;
 			a.addEventListener('click', () => {
-				clearAll();
-				renderPost(getPost(el.id))
+				getComments(post.id)
+					.then((comments) => {
+						renderPost(post, author, comments);
+					})
 			});
 
 			li.appendChild(a);
 			ul.appendChild(li);
 		});
-		body.appendChild(ul);
+
+		return ul;
 	}
 
-	function renderPost(post){
-		let title = document.createElement('h2');
-		let body = document.createElement('p');
-		let container = document.createElement('div');
-		title.innerText = post.title;
-		body.innerText = post.body;
-		container.appendChild(title);
-		container.appendChild(body);
-		body.appendChild(container);
-		renderComments(getComments(post.id));
+	function createPostElement(post, author, comments) {
+		const content = document.createElement('div');
+		content.innerHTML = `
+			<h3>${post.title}</h3>
+			<p>Author: <i>${author.name}</i></p>
+			<p>${post.body}</p>
+		`;
+
+		const commentsList = createCommentsList(comments);
+		content.appendChild(commentsList);
+
+		return content;
 	}
 
-	function renderComments(comments){
-		let container = document.createElement('ul');
-		comments.map(el => {
-			let comment = document.createElement('li');
-			let title = document.createElement('h2');
-			let body = document.createElement('p');
-			let author = document.createElement('h4');
+	function createCommentsList(comments){
+		const content = document.createElement('ul');
+		content.innerHTML = `
+			<li><h2>Comments:</h2></li>
+		`;
+		comments.map(comment => {
+			const commentEl = document.createElement('li');
+			commentEl.innerHTML = `
+				<h4>${comment.name}</h4>
+				<p>Author: <i>${comment.email}</i></p>
+				<p>${comment.body}</p>
+			`;
 
-			title.innerText = el.title;
-			body.innerText = el.body;
-			comment.appendChild(title);
-			comment.appendChild(body);
-			container.appendChild(comment);
-			body.appendChild(container);
-		})
+			content.appendChild(commentEl);
+		});
+
+		return content;
 	}
 
-	function getPost(id){
-		fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8',
-			}
-		})
-			.then(res => res.json() )
-			.then(res => renderPost(res));
-	}
+	// HTTP calls
 
 	function getUsers(){
-		fetch(`https://jsonplaceholder.typicode.com/users`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8',
-			}
-		})
-			.then(res => res.json() )
-			.then(res => localStorage.setItem('users',JSON.stringify(res)));
+		return usersData
+			? Promise.resolve(usersData)
+			: fetch(`https://jsonplaceholder.typicode.com/users`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8',
+				}
+			})
+				.then(res => res.json() )
+				.then(res => {
+					usersData = res.reduce((acc, user) => {
+						return {...acc, [user.id]: user }
+					}, {});
+
+					return usersData;
+				});
 	}
 
 	function getPosts(){
-		fetch('https://jsonplaceholder.typicode.com/posts', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8',
-			}
-		})
-			.then(res => res.json() )
-			.then(res => localStorage.setItem('posts',JSON.stringify(res)));
+		return postsData
+			? Promise.resolve(postsData)
+			: fetch('https://jsonplaceholder.typicode.com/posts', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json;charset=utf-8',
+				}
+			})
+				.then(res => res.json())
+				.then(posts => {
+					postsData = posts;
+					return postsData;
+				});
 	}
 
 	function getComments(id){
-		fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`, {
+		return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`, {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json;charset=utf-8',
 			}
 		})
-			.then(res => res.json() )
+			.then(res => res.json())
 			.then(res => res);
 	}
-
-	function clearAll(){
-		body.innerHTML = '';
-	}
-
-
 });
